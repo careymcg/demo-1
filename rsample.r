c0feb83207ce48a38a72c96917bb8a5babcf714d
awhitten <awhitten@gmail.com>
2014-06-09 13:59:13 -0700

    Fix line errors

--- rsample.r
+++ rsample.r
@@ -8,9 +8,9 @@ There are two sections of code.
 
 plot(table(rpois(100, 5)), type = "h", col = "red", lwd = 10, main = "rpois(100, lambda = 5)")
 
-## Simple Quantiles/ECDF
+### Simple Quantiles/ECDF
 
 plot(x <- sort(rnorm(47)), type = "s", main = "plot(x, type = \"s\")")
 points(x, cex = .5, col = "dark red")
 
-This is the end of the file.
+# This is the end of the file.
